6f9f4a0fa9b8de1b084ed05968e1faab1fc26bf0
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2021-08-25 19:32:45 +0300

    Create functions for editing contacts

---
+++ contacts.js
@@ -0,0 +1,64 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const shortid = require("shortid");
+
+const contactsPath = path.resolve("db", "./contacts.json");
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const getContactById = async (id) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const userMatch = contacts.find((contact) => contact.id === id);
+    console.log(userMatch);
+    if (!userMatch) {
+      throw new Error(`Contact with id:${id} not found`);
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const removeContact = async (id) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const contactIdx = contacts.findIndex((contact) => contact.id === id);
+    if (contactIdx === -1) {
+      throw new Error(`Contact with id:${id} not found`);
+    }
+    const filteredContacts = contacts.filter((contact) => contact.id !== id);
+    console.log(filteredContacts);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const addContact = async (contact) => {
+  try {
+    const newContact = { id: shortid.generate(), ...contact };
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    const updatedContacts = [...contacts, newContact];
+    console.log(updatedContacts);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
